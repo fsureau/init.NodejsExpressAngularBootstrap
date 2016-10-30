25b826a8a72b2718fb51d737dd13d9066a24d505
florian sureau <florian.sureau@gmail.com>
2016-10-30 19:01:33 +0100

    changing style for bootstrap

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -1,6 +1,7 @@
 angular.module('mainModule', [])
     .controller('defaultController', function () {
         var data = this;
+        data.inputText = "My first application";
         data.elements = [
             { text: 'element1'},
             { text: 'element2'}];
